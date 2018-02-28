7a8a3f4f06f3b16b6306f605d2429508f557833a
Kadaiser <dvalbuen@ucm.es>
2018-02-28 19:15:19 +0000

    v 1.0.0.20180228

--- bot.js
+++ bot.js
@@ -1,44 +1,36 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+//load discord.js library
+const Discord = require('discord.js');
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+//Generate bot
+const bot = new Discord.Client();
+
+//Get private token
+const auth = require("./auth.json");
+
+//over every single message recieved
+bot.on("message", async message => {
+	//(block "botception").
+	if(message.author.bot) return;
+
+	//Epic & pretty demanded función in every bot
+	if (message.content === 'ping')
+    		message.reply('pong');
+
+	//ignore any command whitout prefix command
+	if(message.content.indexOf(auth.prefix) !== 0) return;
+
+	//Ussage prefix[command] [argv0]...[argvN]
+	const args = message.content.slice(auth.prefix.length).trim().split(/ +/g);
+  	//Parse command to standar type
+	const command = args.shift().toLowerCase();
+
+	//echo command
+	if(command === "echo") {
+	const echo = args.join(" ");
+	message.delete().catch(O_o=>{});
+	message.channel.send(echo);
+	}
 });
 
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-        args = args.splice(1);
-
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-
-            // Just add any case commands if you want to..
-         }
-     }
-});
+bot.login(auth.token);
