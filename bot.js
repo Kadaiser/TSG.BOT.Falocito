ac3c769faa9dbce6ae1d212ff3f3d7bc12ff0a7c
Kadaiser <dvalbuen@ucm.es>
2018-03-01 17:26:57 +0000

    v 1.0.1.20180301

--- bot.js
+++ bot.js
@@ -5,10 +5,18 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 //Get private token
-const auth = require("./auth.json");
+const auth = require('./auth.json');
+
+//Bot Boot Sequence (BBS)
+bot.on('ready', function(){
+	console.log('Falocito en marcha!');
+});
 
 //over every single message recieved
-bot.on("message", async message => {
+bot.on('message', async function(message){
+	// Turns message it all into uppercase so it isn't case sensitive.
+	let msg = message.content.toUpperCase(); 
+
 	//(block "botception").
 	if(message.author.bot) return;
 
@@ -19,17 +27,35 @@ bot.on("message", async message => {
 	//ignore any command whitout prefix command
 	if(message.content.indexOf(auth.prefix) !== 0) return;
 
+
+
 	//Ussage prefix[command] [argv0]...[argvN]
 	const args = message.content.slice(auth.prefix.length).trim().split(/ +/g);
-  	//Parse command to standar type
+	//delete mesagge 
+	message.delete().catch(function(){});
+  
+	//Parse command to standar type
 	const command = args.shift().toLowerCase();
 
 	//echo command
 	if(command === "echo") {
-	const echo = args.join(" ");
-	message.delete().catch(O_o=>{});
-	message.channel.send(echo);
+		const echo = args.join(" ");
+		message.channel.send(echo);
 	}
+	if(command == "a"){
+		const insult = args[0];
+		message.channel.send(insult+' es gilipollas!');
+	}
+
+	if(command == "arzu"){
+                message.channel.send('La culpa es de Marzu');
+        }
+
+	if(command == "ol"){
+		var result = Math.floor(6*Math.random())+1;
+                message.channel.send(message.author + " saco un " + result);
+        }
+
 });
 
 
